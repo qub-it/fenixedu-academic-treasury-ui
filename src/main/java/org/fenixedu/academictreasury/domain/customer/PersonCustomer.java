2d0e0d9d560f7efd618517dbbd2f58fabc27527a
Ricardo Pedro <ricardo@soferragens.pt>
2015-06-11 15:21:28 +0100

    - Correction for PersonCustomer Import

--- src/main/java/org/fenixedu/academictreasury/domain/customer/PersonCustomer.java
+++ src/main/java/org/fenixedu/academictreasury/domain/customer/PersonCustomer.java
@@ -9,38 +9,48 @@ import org.fenixedu.treasury.domain.Customer;
 
 import pt.ist.fenixframework.Atomic;
 
+import com.google.common.base.Strings;
 
 public class PersonCustomer extends PersonCustomer_Base {
-    
+
     protected PersonCustomer() {
         super();
     }
-    
+
     protected PersonCustomer(final Person person) {
         this();
-        
+
+        this.init(person);
+    }
+
+    protected void init(Person person) {
         setPerson(person);
-        
+
         checkRules();
     }
 
-    private void checkRules() {
-        if(getPerson() == null) {
+    @Override
+    protected void checkRules() {
+        super.checkRules();
+        if (getPerson() == null) {
             throw new AcademicTreasuryDomainException("error.PersonCustomer.person.required");
         }
-        
-        if(find(getPerson()).count() > 1) {
+
+        if (find(getPerson()).count() > 1) {
             throw new AcademicTreasuryDomainException("error.PersonCustomer.person.customer.duplicated");
         }
     }
 
     @Override
     public String getCode() {
-        return getFiscalNumber();
+        return getExternalId();
     }
 
     @Override
     public String getFiscalNumber() {
+        if (Strings.isNullOrEmpty(getPerson().getSocialSecurityNumber())) {
+            return getPerson().getDocumentIdNumber();
+        }
         return getPerson().getSocialSecurityNumber();
     }
 
@@ -52,7 +62,7 @@ public class PersonCustomer extends PersonCustomer_Base {
     public String getIdentificationNumber() {
         return getPerson().getDocumentIdNumber();
     }
-    
+
     @Override
     public String getAddress() {
         return null;
@@ -60,28 +70,29 @@ public class PersonCustomer extends PersonCustomer_Base {
 
     @Override
     public String getDistrictSubdivision() {
-        if(getPerson().getDefaultPhysicalAddress() == null) {
+        if (getPerson().getDefaultPhysicalAddress() == null) {
             return null;
         }
-        
+
         return getPerson().getDefaultPhysicalAddress().getAddress();
     }
 
     @Override
     public String getZipCode() {
-        if(getPerson().getDefaultPhysicalAddress() == null) {
+        if (getPerson().getDefaultPhysicalAddress() == null) {
             return null;
         }
-                
+
         return getPerson().getDefaultPhysicalAddress().getAreaCode();
     }
 
     @Override
     public String getCountryCode() {
-        if(getPerson().getDefaultPhysicalAddress() == null && getPerson().getDefaultPhysicalAddress().getCountryOfResidence() != null) {
+        if (getPerson().getDefaultPhysicalAddress() == null
+                && getPerson().getDefaultPhysicalAddress().getCountryOfResidence() != null) {
             return null;
         }
-        
+
         return getPerson().getDefaultPhysicalAddress().getCountryOfResidence().getThreeLetterCode();
     }
 
@@ -89,12 +100,12 @@ public class PersonCustomer extends PersonCustomer_Base {
     public String getPaymentReferenceBaseCode() {
         return this.getCode();
     }
-    
+
     @Override
     public boolean isPersonCustomer() {
         return true;
     }
-    
+
     // @formatter: off
     /************
      * SERVICES *
@@ -104,18 +115,21 @@ public class PersonCustomer extends PersonCustomer_Base {
     public static Stream<? extends PersonCustomer> findAll() {
         return Customer.findAll().filter(c -> c instanceof PersonCustomer).map(PersonCustomer.class::cast);
     }
-    
+
     protected static Stream<? extends PersonCustomer> find(final Person person) {
         return findAll().filter(pc -> pc.getPerson() == person);
     }
-    
+
     public static Optional<? extends PersonCustomer> findUnique(final Person person) {
         return PersonCustomer.findAll().filter(pc -> pc.getPerson() == person).findFirst();
     }
-    
+
     @Atomic
     public static PersonCustomer create(final Person person) {
         return new PersonCustomer(person);
     }
-    
+
+    public static Optional<? extends PersonCustomer> findByFiscalNumber(String fiscalNumber) {
+        return PersonCustomer.findAll().filter(pc -> pc.getFiscalNumber().equals(fiscalNumber)).findFirst();
+    }
 }
