c01eacd6671c5779da6d0f2b9f9f5691966a13dd
Ricardo Pedro <ricardo@soferragens.pt>
2015-09-09 00:50:09 +0100

    DISABLE logging FOR TESTING LOCK's ON DATABASE

--- src/main/java/org/fenixedu/academictreasury/domain/debtGeneration/AcademicDebtGenerationRule.java
+++ src/main/java/org/fenixedu/academictreasury/domain/debtGeneration/AcademicDebtGenerationRule.java
@@ -369,18 +369,19 @@ public class AcademicDebtGenerationRule extends AcademicDebtGenerationRule_Base 
         processDebtsForRegistration(registration, logBean);
     }
 
-    @Atomic(mode = TxMode.WRITE)
+//HACK: RSP DISABLE FOR TESTING LOCK's ON DATABASE
+    //@Atomic(mode = TxMode.WRITE)
     private void writeLog(final LogBean logBean) {
-        int MAX_LENGTH = 32 * 1024;
-        int length = logBean.log.length();
-        if (length <= MAX_LENGTH) {
-            TreasuryOperationLog.create(logBean.log.toString(), this.getExternalId(), TREASURY_OPERATION_LOG_TYPE);
-        } else {
-            List<String> splitToList = Splitter.fixedLength(MAX_LENGTH).splitToList(logBean.log.toString());
-            for (String str : splitToList) {
-                TreasuryOperationLog.create(str, this.getExternalId(), TREASURY_OPERATION_LOG_TYPE);
-            }
-        }
+        //int MAX_LENGTH = 32 * 1024;
+        //int length = logBean.log.length();
+        //if (length <= MAX_LENGTH) {
+      //      TreasuryOperationLog.create(logBean.log.toString(), this.getExternalId(), TREASURY_OPERATION_LOG_TYPE);
+        //} else {
+        //    List<String> splitToList = Splitter.fixedLength(MAX_LENGTH).splitToList(logBean.log.toString());
+        //    for (String str : splitToList) {
+        //        TreasuryOperationLog.create(str, this.getExternalId(), TREASURY_OPERATION_LOG_TYPE);
+        //    }
+        //}
     }
 
     @Atomic(mode = TxMode.WRITE)
