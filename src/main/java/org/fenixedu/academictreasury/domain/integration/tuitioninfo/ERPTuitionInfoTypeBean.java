7ef48d6203b9bf91f48a20eb603db6cf14dcd8dc
anilmamede <anil.mamede@qub-it.com>
2018-04-10 10:43:47 +0100

    Fix import

--- src/main/java/org/fenixedu/academictreasury/domain/integration/tuitioninfo/ERPTuitionInfoTypeBean.java
+++ src/main/java/org/fenixedu/academictreasury/domain/integration/tuitioninfo/ERPTuitionInfoTypeBean.java
@@ -5,9 +5,8 @@ import java.io.Serializable;
 import org.fenixedu.academic.domain.Degree;
 import org.fenixedu.academic.domain.DegreeCurricularPlan;
 import org.fenixedu.academic.domain.degree.DegreeType;
+import org.fenixedu.commons.i18n.LocalizedString;
 import org.fenixedu.treasury.dto.ITreasuryBean;
-
-import com.sun.tools.javac.api.Formattable.LocalizedString;
 
 public class ERPTuitionInfoTypeBean implements ITreasuryBean, Serializable {
 
