b32204c35b326367c37414723892b173a628cbe2
Anil Mamede <anil.mamede@qub-it.com>
2024-06-25 17:08:28 +0100

    - Refactor: Remove usage of AdministrativeOffice relations [#qubIT-Fenix-5448]

--- src/main/java/org/fenixedu/academictreasury/domain/treasury/DefaultAcademicTreasuryAccountUrl.java
+++ src/main/java/org/fenixedu/academictreasury/domain/treasury/DefaultAcademicTreasuryAccountUrl.java
@@ -3,12 +3,15 @@ package org.fenixedu.academictreasury.domain.treasury;
 import org.fenixedu.academic.domain.Person;
 import org.fenixedu.academic.domain.student.Registration;
 import org.fenixedu.academictreasury.domain.customer.PersonCustomer;
+import org.fenixedu.academictreasury.services.AcademicTreasuryPlataformDependentServicesFactory;
+import org.fenixedu.treasury.domain.FinantialEntity;
 import org.fenixedu.treasury.domain.FinantialInstitution;
 import org.fenixedu.treasury.domain.debt.DebtAccount;
 import org.fenixedu.treasury.domain.document.DebitEntry;
 import org.fenixedu.treasury.domain.event.TreasuryEvent;
 import org.fenixedu.treasury.ui.accounting.managecustomer.CustomerController;
 import org.fenixedu.treasury.ui.accounting.managecustomer.DebtAccountController;
+import org.joda.time.LocalDate;
 
 public class DefaultAcademicTreasuryAccountUrl implements IAcademicTreasuryAccountUrl {
 
@@ -16,18 +19,19 @@ public class DefaultAcademicTreasuryAccountUrl implements IAcademicTreasuryAccou
     public String getPersonAccountTreasuryManagementURL(Person person) {
         final String countryCode = PersonCustomer.addressCountryCode(person);
         final String fiscalNumber = PersonCustomer.fiscalNumber(person);
-        return CustomerController.READ_URL
-                + PersonCustomer.findUnique(person, countryCode, fiscalNumber).get().getExternalId();
+        return CustomerController.READ_URL + PersonCustomer.findUnique(person, countryCode, fiscalNumber).get().getExternalId();
     }
 
     @Override
     public String getRegistrationAccountTreasuryManagementURL(Registration registration) {
-        if (registration.getDegree().getAdministrativeOffice().getFinantialEntity() == null) {
+        FinantialEntity finantialEntity = AcademicTreasuryPlataformDependentServicesFactory.implementation()
+                .finantialEntityOfDegree(registration.getDegree(), new LocalDate());
+
+        if (finantialEntity == null) {
             return getPersonAccountTreasuryManagementURL(registration.getPerson());
         }
 
-        final FinantialInstitution inst =
-                registration.getDegree().getAdministrativeOffice().getFinantialEntity().getFinantialInstitution();
+        final FinantialInstitution inst = finantialEntity.getFinantialInstitution();
         final Person person = registration.getPerson();
         final String countryCode = PersonCustomer.addressCountryCode(person);
         final String fiscalNumber = PersonCustomer.fiscalNumber(person);
@@ -36,8 +40,7 @@ public class DefaultAcademicTreasuryAccountUrl implements IAcademicTreasuryAccou
 
         final DebtAccount account = customer.getDebtAccountFor(inst);
         if (account != null) {
-            return DebtAccountController.READ_URL
-                    + customer.getDebtAccountFor(inst).getExternalId();
+            return DebtAccountController.READ_URL + customer.getDebtAccountFor(inst).getExternalId();
         } else {
             return getPersonAccountTreasuryManagementURL(person);
         }
